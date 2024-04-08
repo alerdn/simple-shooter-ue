15cb5d1c780b7071f281339a7e643c54a7bb10bf
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2024-04-08 12:38:11 -0400

    feat: mostrando tela de derrota

--- Source/SimpleShooter/ShooterPlayerController.cpp
+++ Source/SimpleShooter/ShooterPlayerController.cpp
@@ -2,10 +2,19 @@
 
 #include "ShooterPlayerController.h"
 #include "TimerManager.h"
+#include "Blueprint/UserWidget.h"
 
 void AShooterPlayerController::GameHasEnded(class AActor *EndGameFocus, bool bIsWinner)
 {
     Super::GameHasEnded(EndGameFocus, bIsWinner);
    
+    // Criamos a UI, mas ainda não está na tela
+    UUserWidget* LoseScreen = CreateWidget(this, LoseScreenClass);
+    if (LoseScreen)
+    {
+        // Adicionamos a UI na tela
+        LoseScreen->AddToViewport();
+    }
+
     GetWorldTimerManager().SetTimer(RestartTimer, this, &APlayerController::RestartLevel, RestartDelay);
 }
